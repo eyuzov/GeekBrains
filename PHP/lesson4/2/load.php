6de0b7bbabb3ff1fc8811102bed95dc02a776bb2
eyuzov <43463867+eyuzov@users.noreply.github.com>
2022-11-29 15:13:05 +0300

    Delete PHP directory

--- PHP/lesson4/2/load.php
+++
@@ -1,5 +0,0 @@
-<?php
-$path = "photo/big/";
-$file = $path.$_GET["file"];
-$img = "<img src='$file'>";
-echo $img;
--- PHP/lesson5/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user = "root";
-$pass = "";
-$db = "homework05";
-$table = "images";
-$host = "localhost";
-
